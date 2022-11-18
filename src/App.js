51231f3a963772eb47b13757c6f26e81d255cb28
AtsushiNi <atsushi9731@gmail.com>
2022-11-18 23:44:42 +0900

    auth

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -10,17 +11,51 @@ import Home from './components/Home'
 import Calendar from './components/Calendar'
 import TestDemo from './components/TestDemo'
 
+import { Auth, Hub } from 'aws-amplify'
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth'
+
 function App() {
+  const [user, setUser] = useState(null)
+  const [customState, setCustomState] = useState(null)
+
+  useEffect(() => {
+    const unsubscribe = Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          setUser(data);
+          break;
+        case "signOut":
+          setUser(null);
+          break;
+        case "customOAuthState":
+          setCustomState(data);
+      }
+    });
+
+    Auth.currentAuthenticatedUser()
+      .then(currentUser => setUser(currentUser))
+      .catch(() => console.log("Not signed in"));
+
+    return unsubscribe;
+  }, [])
+
+  console.log(user)
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path='/signin' element={<SignIn />} />
-          <Route path="/calendar" element={<Calendar />} />
-          {/* <Route path='/test' element={<TestDemo />} /> */}
-        </Routes>
-      </Router>
+    <div>
+      <div className="App">
+        <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+        <button onClick={() => Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google })}>Open Google</button>
+        <button onClick={() => Auth.signOut()}>Sign Out</button>
+        <div>{user && user.getUsername()}</div>
+        <Router>
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path='/signin' element={<SignIn />} />
+            <Route path="/calendar" element={<Calendar />} />
+            {/* <Route path='/test' element={<TestDemo />} /> */}
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
